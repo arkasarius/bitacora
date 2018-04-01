768638c93c7f134f34f989cac089f7faa2692d4e
Torre/Asarius <roger.ordonez.brumos@gmail.com>
2018-04-01 19:18:19 +0200

    added get time and getdiamesany for functionality

--- app/src/main/java/ordonez/roger/bitacoraactivity/BitacoraActivity.java
+++ app/src/main/java/ordonez/roger/bitacoraactivity/BitacoraActivity.java
@@ -4,7 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import java.sql.Date;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class BitacoraActivity extends AppCompatActivity {
     private ArrayList<BitacoraItem> items;
@@ -15,7 +18,9 @@ public class BitacoraActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bitacora);
         items = new ArrayList<>();
-        PopulateItem("soc jo","12:25","12/12/2018");
+        //PopulateItem("soc jo","12:25","12/12/2018");
+
+        PopulateItem("text", GetCurrentTime(),GetCurrentDiaMesAny());
         list = findViewById(R.id.EditableID);
         adapter = new AdapterBitacoraItem(this,R.layout.activity_bitacor_item, items);
         list.setAdapter(adapter);
@@ -27,5 +32,23 @@ public class BitacoraActivity extends AppCompatActivity {
 
     public void PopulateItem(String tasca,String hora,String diamesany){
         items.add(new BitacoraItem(tasca,hora,diamesany));
+    }
+    public String GetCurrentTime(){
+        String a="";
+        Calendar calendar = new GregorianCalendar();
+        a=Integer.toString(calendar.get(Calendar.HOUR_OF_DAY))+":"+Integer.toString(calendar.get(Calendar.MINUTE));
+        return a;
+    }
+    public String GetCurrentDiaMesAny(){
+        String a="";
+        Calendar calendar = new GregorianCalendar();
+        a=
+                 Integer.toString(calendar.get(Calendar.DAY_OF_WEEK))
+                         +"/"
+                +Integer.toString(calendar.get(Calendar.MONTH))
+                         +"/"
+                +Integer.toString(calendar.get(Calendar.YEAR))
+        ;//day/month/year format
+        return a;
     }
 }
